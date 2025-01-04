5ea7eb66d7ac7d1644e61f13b5cd9239ffc63c68
Chris Demetriad <chris@demetriad.co.uk>
2025-01-04 16:01:09 +0000

    updating homepage text

--- app/index.tsx
+++ app/index.tsx
@@ -7,25 +7,17 @@ const index = () => {
 		<View>
 			<Text>Welcome</Text>
 			<Text>Your free practice tests</Text>
+			<Text>Our platform offers a comprehensive set of practice tests designed to help you excel in your studies.</Text>
+			<Text>With a user-friendly interface and diverse question sets, you can practice anytime, anywhere.</Text>
 			<Text>
-				Our platform offers a comprehensive set of practice tests designed to
-				help you excel in your studies.
+				Accessible Anytime – take practice tests at your convenience, without the constraints of location or time.
 			</Text>
 			<Text>
-				With a user-friendly interface and diverse question sets, you can
-				practice anytime, anywhere.
+				Diverse Question Sets – access a wide variety of questions across multiple subjects and difficulty levels.
 			</Text>
 			<Text>
-				Accessible Anytime – take practice tests at your convenience, without
-				the constraints of location or time.
-			</Text>
-			<Text>
-				Diverse Question Sets – access a wide variety of questions across
-				multiple subjects and difficulty levels.
-			</Text>
-			<Text>
-				Instant Feedback – receive immediate results and performance analytics
-				to help identify strengths and weaknesses.
+				Instant Feedback – receive immediate results and performance analytics to help identify strengths and
+				weaknesses.
 			</Text>
 
 			<Link href="/home">Next screen!</Link>
