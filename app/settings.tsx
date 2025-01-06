55052321e6b95d83b44148938e15f1721a308884
Chris Demetriad <chris@demetriad.co.uk>
2025-01-06 21:48:11 +0000

    adding basic settings screen

---
+++ app/settings.tsx
@@ -0,0 +1,21 @@
+import { View, Text, Button } from "react-native";
+import { useRouter } from "expo-router";
+import { supabase } from "@lib/supabase";
+
+const settings = () => {
+	const router = useRouter();
+
+	const handleLogout = async () => {
+		await supabase.auth.signOut();
+		router.replace("/");
+	};
+
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Settings</Text>
+			<Button title="Log Out" onPress={handleLogout} />
+		</View>
+	);
+};
+
+export default settings;
