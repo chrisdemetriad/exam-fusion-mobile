9b37fe3d2a1ce2f51d54c8a1b2ed9cd279439a68
Chris Demetriad <chris@demetriad.co.uk>
2025-01-06 15:23:04 +0000

    adding tabs icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,47 +1,67 @@
 import { Tabs } from "expo-router";
 import AuthGuard from "@components/AuthGuard";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default () => {
 	return (
 		<AuthGuard>
 			<Tabs
 				screenOptions={{
+					tabBarStyle: {
+						paddingHorizontal: 0,
+					},
 					tabBarLabelStyle: {
 						textTransform: "uppercase",
+						fontSize: 9,
 					},
-					headerTitleStyle: {
-						textTransform: "uppercase",
+					tabBarIconStyle: {
+						marginBottom: 4,
 					},
 				}}
 			>
 				<Tabs.Screen
-					name="home"
-					options={{
-						title: "Home",
-					}}
-				/>
-				<Tabs.Screen
 					name="practice"
 					options={{
 						title: "Practice",
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="book-outline" size={18} color={color} />
+						),
 					}}
 				/>
 				<Tabs.Screen
 					name="progress"
 					options={{
 						title: "Progress",
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="bar-chart-outline" size={18} color={color} />
+						),
 					}}
 				/>
 				<Tabs.Screen
 					name="leaderboard"
 					options={{
 						title: "Leaderboard",
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="trophy-outline" size={18} color={color} />
+						),
 					}}
 				/>
 				<Tabs.Screen
 					name="help"
 					options={{
 						title: "Help",
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="help-circle-outline" size={18} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="settings"
+					options={{
+						title: "Settings",
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="settings-outline" size={18} color={color} />
+						),
 					}}
 				/>
 			</Tabs>
