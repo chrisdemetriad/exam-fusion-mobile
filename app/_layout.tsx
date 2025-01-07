677a9f09906597c3193d52f506ec7065e85ad74c
Chris Demetriad <chris@demetriad.co.uk>
2025-01-07 01:12:09 +0000

    adding fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-native-url-polyfill/auto";
-import { useEffect } from "react";
 import { supabase } from "@lib/supabase";
 import { useAuthStore } from "@store/authStore";
-import { View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
 
 const StackLayout = () => {
 	const { setSession, setLoading } = useAuthStore();
+	const [fontsLoaded] = useFonts({
+		"Mulish-Regular": require("../assets/fonts/Mulish-Regular.ttf"),
+		"Comfortaa-Regular": require("../assets/fonts/Comfortaa-Regular.ttf"),
+	});
 
 	useEffect(() => {
 		supabase.auth.getSession().then(({ data: { session } }) => {
@@ -23,9 +28,24 @@ const StackLayout = () => {
 		});
 
 		return () => subscription.unsubscribe();
-	}, []);
+	}, [setSession, setLoading]);
 
 	const client = new QueryClient();
+
+	if (!fontsLoaded) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#f0f0f0",
+				}}
+			>
+				<ActivityIndicator size="large" color="#000" />
+			</View>
+		);
+	}
 
 	return (
 		<QueryClientProvider client={client}>
@@ -39,6 +59,9 @@ const StackLayout = () => {
 					screenOptions={{
 						headerStyle: { backgroundColor: "#f0f0f0" },
 						contentStyle: { backgroundColor: "#f0f0f0" },
+						headerTitleStyle: {
+							fontFamily: "Mulish-Regular",
+						},
 					}}
 				>
 					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
