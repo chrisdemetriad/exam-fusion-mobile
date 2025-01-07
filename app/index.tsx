df3a6c9271f93824e0025c6f0e81537a52214b79
Chris Demetriad <chris@demetriad.co.uk>
2025-01-07 01:12:22 +0000

    getting rid of unneeded link

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,10 @@
 import { View } from "react-native";
-import { Link, useRouter } from "expo-router";
 import Auth from "@components/auth";
-import { useAuthStore } from "@store/authStore";
 
 const index = () => {
-	const { session } = useAuthStore();
-	const router = useRouter();
-
-	const handleSkip = () => {
-		if (!session) {
-			alert("Please login first");
-			return;
-		}
-		router.push("/practice");
-	};
-
 	return (
 		<View>
 			<Auth />
-			<Link
-				href="/practice"
-				onPress={(e) => {
-					e.preventDefault();
-					handleSkip();
-				}}
-			>
-				Skip
-			</Link>
 		</View>
 	);
 };
